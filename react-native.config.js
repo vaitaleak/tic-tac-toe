46cc615e4b233062f9abd7439816bfae896a031e
vaitaleak <vaitaleak@users.noreply.github.com>
2026-05-09 18:31:40 +0200

    fix: restore autolink with react-native.config.js

--- react-native.config.js
+++ react-native.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   project: {
     android: {
-      unstable_reactLegacyComponentNames: ['RCTImageView'],
+      packageName: 'com.usefullapps.tictactoe',
+      sourceDir: './android',
+      appName: 'tictactoe',
+      manifestPath: './app/src/main/AndroidManifest.xml',
     },
+    ios: {},
   },
 };
